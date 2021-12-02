fc7567fc12aa4fb2837b0961065dfe5746af312c
gabriel <santosgabriel6763@gmail.com>
2021-12-02 21:25:07 -0200

    Resize images

--- script.js
+++ script.js
@@ -3,13 +3,10 @@ var chk = window.document.getElementById("check")
 var MenuLateral = window.document.getElementById("menu-lateral")
 
 function FuncAbrirMenuLateral(){
-    var caixa = window.document.getElementsByTagName("label")[0]
     if(chk.checked){
         MenuLateral.style.width="0%"
-        caixa.style.background="#B0C4DE"
     }else{
         MenuLateral.style.width="100%"
-        caixa.style.background="#EEE8AA"
     }
 }
 
